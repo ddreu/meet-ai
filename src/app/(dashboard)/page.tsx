51fafc080b97ef7602b9121ee19fbe144125eda4
Andrew <3922017@holycross.edu.ph>
2025-09-10 04:19:02 +0800

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,14 +2,19 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
+import { caller } from "@/trpc/server";
 
 const page = async () => {
+  const data = await caller.hello({ text: "Andrew Server" });
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session) {
     redirect("/sign-in");
   }
+
+  return <p>{data.greeting}</p>;
 
   return <HomeView />;
 };
